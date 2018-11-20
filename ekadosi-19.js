a5892cc0878b93d9a5851a724fcff3c506b268f1
Pranta Saha <pranta1204@gmail.com>
2018-11-20 19:42:18 +0600

    2019

---
+++ ekadosi-19.js
@@ -0,0 +1,202 @@
+const ekadosi_data=[
+{
+	"bn_name":"সফলা একাদশী",
+	"en_date":"02.01.2019",
+	"day_name":"বুধবার",
+	"bn_date":"০২.০১.২০১৯",
+	"breakfast":"০৬.৪১-১০.১৫"
+},
+{
+	"bn_name":"পুত্রদা একাদশী",
+	"en_date":"17.01.2019",
+	"day_name":"বৃহস্পতিবার",
+	"bn_date":"১৭.০১.২০১৯",
+	"breakfast":"০৬.৪২-১০.২০"
+},
+{
+	"bn_name":"ষটতিলা একাদশী",
+	"en_date":"31.01.2019",
+	"day_name":"বৃহস্পতিবার",
+	"bn_date":"৩১.০১.২০১৯",
+	"breakfast":"০৬.৩৯-১০.২১"
+},
+{
+	"bn_name":"ভৈমী একাদশী",
+	"en_date":"16.02.2019",
+	"day_name":"শনিবার",
+	"bn_date":"১৬.০২.২০১৯",
+	"breakfast":"০৬.৩০-০৮.৪১"
+},
+{
+	"bn_name":"বিজয়া একাদশী",
+	"en_date":"02.03.2019",
+	"day_name":"শনিবার",
+	"bn_date":"০২.০৩.২০১৯",
+	"breakfast":"০৬.১৯-১০.১৩"
+},
+{
+	"bn_name":"আমলকীব্রত একাদশী",
+	"en_date":"17.03.2019",
+	"day_name":"রবিবার",
+	"bn_date":"১৭.০৩.২০১৯",
+	"breakfast":"০৬.০৫-১০.০৬"
+},
+{
+	"bn_name":"পাপমোচনী একাদশী",
+	"en_date":"01.04.2019",
+	"day_name":"সোমবার",
+	"bn_date":"০১.০৪.২০১৯",
+	"breakfast":"০৫.৫০-০৯.১০"
+},
+{
+	"bn_name":"কামদা একাদশী",
+	"en_date":"16.04.2019",
+	"day_name":"মঙ্গলবার",
+	"bn_date":"১৬.০৪.২০১৯",
+	"breakfast":"০৫.৩৬-০৯.৫০"
+},
+{
+	"bn_name":"বরুথিনী একাদশী",
+	"en_date":"30.04.2019",
+	"day_name":"মঙ্গলবার",
+	"bn_date":"৩০.০৪.২০১৯",
+	"breakfast":"০৭.১৬-০৯.৪৫"
+},
+{
+	"bn_name":"মোহিনী একাদশী",
+	"en_date":"15.05.2019",
+	"day_name":"বুধবার",
+	"bn_date":"১৫.০৫.২০১৯",
+	"breakfast":"০৫.১৬-০৮.৪৭"
+},
+{
+	"bn_name":"অপরা একাদশী",
+	"en_date":"30.05.2019",
+	"day_name":"বৃহস্পতিবার",
+	"bn_date":"৩০.০৫.২০১৯",
+	"breakfast":"০৫.১১-০৯.৪১"
+},
+{
+	"bn_name":"পাণ্ডবা নির্জলা একাদশী",
+	"en_date":"13.06.2019",
+	"day_name":"বৃহস্পতিবার",
+	"bn_date":"১৩.০৬.২০১৯",
+	"breakfast":"০৫.১০-০৯.৪২"
+},
+{
+	"bn_name":"যোগিনী একাদশী",
+	"en_date":"29.06.2019",
+	"day_name":"শনিবার",
+	"bn_date":"২৯.০৬.২০১৯",
+	"breakfast":"০৫.১৪-০৬.৪৩"
+},
+{
+	"bn_name":"শয়ন একাদশী",
+	"en_date":"12.07.2019",
+	"day_name":"শুক্রবার",
+	"bn_date":"১২.০৭.২০১৯",
+	"breakfast":"০৭.০২-০৯.৪৯"
+},
+{
+	"bn_name":"কামিকা একাদশী",
+	"en_date":"28.07.2019",
+	"day_name":"রবিবার",
+	"bn_date":"২৮.০৭.২০১৯",
+	"breakfast":"০৫.২৬-০৯.৫১"
+},
+{
+	"bn_name":"পবিত্রারোপণ একাদশী",
+	"en_date":"11.08.2019",
+	"day_name":"রবিবার",
+	"bn_date":"১১.০৮.২০১৯",
+	"breakfast":"০৫.৩২-০৯.৫৩"
+},
+{
+	"bn_name":"অন্নদা একাদশী",
+	"en_date":"27.08.2019",
+	"day_name":"মঙ্গলবার",
+	"bn_date":"২৭.০৮.২০১৯",
+	"breakfast":"০৫.৩৮-০৯.৫২"
+},
+{
+	"bn_name":"পার্শ্ব একাদশী",
+	"en_date":"09.09.2019",
+	"day_name":"সোমবার",
+	"bn_date":"০৯.০৯.২০১৯",
+	"breakfast":"০৭.৩৬-০৯.৫১"
+},
+{
+	"bn_name":"ইন্দিরা একাদশী",
+	"en_date":"25.09.2019",
+	"day_name":"বুধবার",
+	"bn_date":"২৫.০৯.২০১৯",
+	"breakfast":"০৫.৪৮-০৯.৪৯"
+},
+{
+	"bn_name":"পাশাঙ্কুশা একাদশী",
+	"en_date":"09.10.2019",
+	"day_name":"বুধবার",
+	"bn_date":"০৯.১০.২০১৯",
+	"breakfast":"০৫.৫৩-০৯.৪৭"
+},
+{
+	"bn_name":"রমা একাদশী",
+	"en_date":"24.10.2019",
+	"day_name":"বৃহস্পতিবার",
+	"bn_date":"২৪.১০.২০১৯",
+	"breakfast":"০৬.০০-০৯.৪৮"
+},
+{
+	"bn_name":"উত্থান একাদশী",
+	"en_date":"08.11.2019",
+	"day_name":"শুক্রবার",
+	"bn_date":"০৮.১১.২০১৯",
+	"breakfast":"০৬.০৮-০৯.৫০"
+},
+{
+	"bn_name":"উৎপন্না একাদশী",
+	"en_date":"23.11.2019",
+	"day_name":"শনিবার",
+	"bn_date":"২৩.১১.২০১৯",
+	"breakfast":"০৬.১৮-০৯.৫৬"
+},
+{
+	"bn_name":"মোক্ষদা একাদশী",
+	"en_date":"08.12.2019",
+	"day_name":"রবিবার",
+	"bn_date":"০৮.১২.২০১৯",
+	"breakfast":"০৬.২৮-১০.০৩"
+},
+{
+	"bn_name":"সফলা একাদশী",
+	"en_date":"22.12.2019",
+	"day_name":"রবিবার",
+	"bn_date":"২২.১২.২০১৯",
+	"breakfast":"০৬.৩৬-১০.১০"
+}
+];
+
+
+// let send = document.getElementById("send");
+// let date1= new Date;
+
+// var ekadosiDate= 
+// [ "2018 01 13", "2018 01 28", "2018 02 11", "2018 02 26", "2018 03 13", "2018 03 27", "2018 04 12", "2018 04 26", "2018 05 11", "2018 05 25", "2018 06 10", "2018 07 09", "2018 07 23", "2018 08 08", "2018 08 22", "2018 09 06", "2018 09 20", "2018 10 05", "2018 10 20", "2018 11 04", "2018 11 19", "2018 12 03", "2018 12 19", "2019 01 02" ];
+// var date2;
+// function checkDate(ekadosi){
+// 	 date2 = new Date(ekadosi);
+// 	 return date1 <= date2;
+// }
+// var result= ekadosiDate.filter(checkDate);
+
+// // alert(ekadosiDate.indexOf(result[0]));
+// let ekadosi_index = ekadosiDate.indexOf(result[0]);
+
+// send.onclick= function(){
+// 	let data = ekadosi_list[ekadosi_index];
+// 	document.getElementById("res").innerHTML= `<tr><td>Ekadosir Name: ${data.name}</td><td>Date: ${data.date}</td><td>Paroner Somoy: ${data.breakfast}</td></tr>`;
+// }
+
+window.onload = function(){
+document.getElementById('tt').innerHTML =`${ekadosi_data.map(ek=> `<tr><td>Ekadosir Name: ${ek.bn_name}</td><td>Date: ${ek.bn_date} -${ek.day_name} </td><td>Paroner Somoy: ${ek.breakfast}</td><td>${ek.en_date}</td></tr>`)}`;
+}
